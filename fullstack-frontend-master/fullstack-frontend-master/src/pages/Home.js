dfb826b438712bb6ae24ef02de4422b455b8ab43
Yasara-Amarakoon <102904936+Yasara-Amarakoon@users.noreply.github.com>
2023-03-20 20:50:11 +0530

    .
    .

---
+++ fullstack-frontend-master/fullstack-frontend-master/src/pages/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom";
+export default function Home() {
+
+    const [users,setUsers]=useState([])
+
+    useEffect(()=>{
+        loadUsers();
+    },[]
+
+    );
+
+    const loadUsers=async()=>{
+        const result=await axios.get("http://localhost:8081/users")
+        setUsers(result.data);
+    }
+  return (
+    <div>
+      <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">FirstName</th>
+      <th scope="col">UserName</th>
+      <th scope="col">Email</th>
+      <th scope="col">Changes</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        users.map((user,index)=>(
+            <tr>
+      <th scope="row" key={user.id}>{index+1}</th>
+      <td>{user.name}</td>
+      <td>{user.username}</td>
+      <td>{user.email}</td>
+      <td>
+        <button className="btn btn-primary mx-2">View</button>  
+        <Link className="btn btn-outline-primary mx-2"
+        to={`/edituser/${user.id}`}
+        >Edit</Link>  
+        <button className="btn btn-danger mx-2">Delete</button>  
+           </td>
+    </tr>
+  ))
+    }
+    
+   
+  </tbody>
+</table>
+        </div>
+        </div>  
+    </div>
+  );
+}
